65cb9c714dd2c034334cfed240044b987e92037c
Saturnino Silva <torpedo77@gmail.com>
2022-03-29 22:13:21 -0300

    primeiro commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,34 @@ form.addEventListener('submit',(evento) => {
     evento.preventDefault();
 });
 
-
 const display = document.querySelector('#display');
+const operationList = ['*','+', '/', '-', '=', '%'];
+
+let numero1 = "";
+let numero2 = "";
 
 const buttonList = document.querySelectorAll('button');
 for (var i = 0, len = buttonList.length; i < len; i++) {
     buttonList[i].addEventListener('click', (evento) => {
         const buttonValue = evento.target.value;
 
-        if(buttonValue != '.'){
-            display.value += buttonValue;
-        } else{
-            if(display.value.indexOf('.') == -1){
+        if(operationList.includes(buttonValue)){
+            console.log(buttonValue);
+            if(buttonValue == 'calcular'){
+                numero2 = display.value;
+                const total = parseFloat(numero1) + parseFloat(numero2);
+                display.value = total;
+            } else {
+                numero1 = display.value;
+                display.value = "";
+            }
+        } else {
+            if(buttonValue != '.'){
                 display.value += buttonValue;
+            } else{
+                if(display.value.indexOf('.') == -1){
+                    display.value += buttonValue;
+                }
             }
         }
     }, false);
